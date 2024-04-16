fc94195ae8a929254e12d0ccf3556cb5eb96c40c
Junghoon Lee <coronarita1991@gmail.com>
2024-04-16 16:27:24 +0900

    [Feature] SQL 데이터베이스 연동 및 CRUD 처리 구현

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import conn # 함수 임포트
+from contextlib import asynccontextmanager
 
 import uvicorn
 
-app = FastAPI()
+# @app.on_event("startup") 의 용례가 바뀜(Deprecated)
+# @app.on_event("startup")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    
+    # Execute to make database
+    conn()
+    yield # discriminate startup and shutdown with keword yield
+    
+    # What to execute after shutdown
+    
+app = FastAPI(lifespan=lifespan)
+
 # 라우트 등록
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+    
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000,
--- models/events.py
+++ models/events.py
@@ -1,14 +1,37 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional, List
 
-class Event(BaseModel):
-    id: int             # id : 자동 생성되는 고유 식별자
+# 기존 모델 클래스를 SQL테이블 클래스로 변경
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True) # id : 자동 생성되는 고유 식별자
     title: str          # title : 이벤트 타이틀
     image: str          # image : 이벤트 이미지 배너의 링크
     description: str    # description : 이벤트 설명
-    tags: List[str]     # tags: 그룹화를 위한 이벤트 태그
+    tags: List[str] = Field(sa_column=Column(JSON)) # tags: 그룹화를 위한 이벤트 태그
     location: str       # location : 이벤트 위치
     
+    model_config = {
+        "arbitrary_types_allowed": True,
+        "json_schema_extra": {
+            "example":
+                {
+                 "title": "FastAPI Book Launch",
+                 "image": "https://linktomyimage.com/image.png",
+                 "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                 "tags": ["python", "fastapi", "book", "launch"],
+                 "location": "Google Meet",   
+                }
+        }
+    }
+
+# UPDATE 처리의 바디 유형으로 추가
+class EventUpdate(SQLModel):
+    title: Optional[str] = None
+    image: Optional[str] = None
+    description: Optional[str] = None
+    tags: Optional[List[str]] = None
+    location: Optional[str] = None
+        
     model_config = {
         "json_schema_extra": {
             "example":
